258cc361b357ba64a6f005ad4b578ebaf432a581
mittulmandhan <conanmittul@gmail.com>
2019-08-09 11:48:30 +0530

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
 const routes: Routes = [
   // redirecting routes for the default url
   // this statement must be put on the top to do so
   { path: '', redirectTo: '/departments', pathMatch: 'full' },
   { path: 'departments', component: DepartmentListComponent},
+  { path: 'departments/:id', component: DepartmentDetailComponent },
   { path: 'employees', component: EmployeeListComponent },
   // wildcard route
   // this is for displaying the message 'Page Not Found' when navigated page does not exist
@@ -22,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent];
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent, DepartmentDetailComponent];
